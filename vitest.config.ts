ce8d7b06ea6d5e022feac525c32c20460915e40c
Elys Maldov <maldovelys@gmail.com>
2025-07-12 11:53:23 +0700

    test: setup vitest with coverage and ui

---
+++ vitest.config.ts
@@ -0,0 +1,28 @@
+import { defineConfig, mergeConfig } from "vitest/config";
+import baseConfig from "./vite.config";
+
+export default mergeConfig(
+  baseConfig,
+  defineConfig({
+    test: {
+      globals: true,
+      environment: "jsdom",
+      coverage: {
+        provider: "istanbul",
+        reportsDirectory: "./coverage",
+        reporter: [
+          "text",
+          "clover",
+          "json",
+          [
+            "html",
+            {
+              subdir: "html",
+            },
+          ],
+        ],
+      },
+      include: ["**/*.test.?(c|m)[jt]s?(x)"],
+    },
+  }),
+);
