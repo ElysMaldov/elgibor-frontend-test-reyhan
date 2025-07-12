83bd1c3d256e336b72e9bb26360b675fccfbeecf
Elys Maldov <maldovelys@gmail.com>
2025-07-12 22:57:40 +0700

    feat: add test "User can query products based on name"

--- src/tests/e2e/user-browse-products.spec.ts
+++ src/tests/e2e/user-browse-products.spec.ts
@@ -9,3 +9,17 @@ test("User can see all products", async ({ page }) => {
   const count = await items.count();
   expect(count).toBeGreaterThan(0);
 });
+
+test("User can query products based on name", async ({ page }) => {
+  await page.goto("http://localhost:5173/");
+
+  await page.getByTestId("searchbar").click();
+  await page.getByTestId("searchbar").fill("Slim");
+
+  await expect(page.getByTestId("list-products")).toContainText(
+    "Mens Casual Premium Slim Fit T-Shirts",
+  );
+  await expect(page.getByTestId("list-products")).toContainText(
+    "Mens Casual Slim Fit",
+  );
+});
