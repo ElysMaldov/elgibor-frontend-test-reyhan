957077c7a589aedf4b0d999592108449a8587c52
Elys Maldov <maldovelys@gmail.com>
2025-07-12 23:14:03 +0700

    test: add test "User can write a review"

--- src/tests/e2e/user-see-product-details.spec.ts
+++ src/tests/e2e/user-see-product-details.spec.ts
@@ -24,3 +24,27 @@ test("User can see a product's detail", async ({ page }) => {
     page.getByText("Customer Reviews0.0(0 reviews)Write a Review"),
   ).toBeVisible();
 });
+
+test("User can write a review", async ({ page }) => {
+  await page.goto("http://localhost:5173/product/1");
+
+  await page.getByRole("button", { name: "Write a Review" }).click();
+  await page.getByRole("textbox", { name: "Name *" }).click();
+  await page.getByRole("textbox", { name: "Name *" }).fill("Reyhan Ardiya");
+  await page.getByRole("textbox", { name: "Email *" }).click();
+  await page
+    .getByRole("textbox", { name: "Email *" })
+    .fill("maldovelys@gmail.com");
+  await page
+    .locator("div")
+    .filter({ hasText: /^Rating \*$/ })
+    .getByRole("button")
+    .nth(3)
+    .click();
+  await page.getByTestId("comment").click();
+  await page.getByTestId("comment").fill("Produk bagus sekali!");
+  await page.getByRole("button", { name: "Submit Review" }).click();
+  await expect(page.locator("h4")).toContainText("Reyhan Ardiya");
+  await expect(page.locator("small")).toContainText("maldovelys@gmail.com");
+  await expect(page.getByRole("main")).toContainText("Produk bagus sekali!");
+});
